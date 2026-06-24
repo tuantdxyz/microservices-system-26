fb11e718f09bb948e3a931b187fa989f4dbf89d4
Jin <tuantrinh@lgcns.com>
2026-06-24 17:49:20 +0700

    init project

---
+++ backend/request-service/src/test/java/com/apartment/request/RequestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apartment.request;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
